4a51799ecd25d20d5d2638ec09f1ec78ccbf371d
vaibhav <vaibs4007@rediff.com>
2016-08-10 18:51:35 +0530

    subject : moving background
    added birds horizontal movement , added bottom and top tubes
    collision of bird restarts the game

---
+++ core/src/com/vaibhav/game/sprites/Tube.java
@@ -0,0 +1,65 @@
+package com.vaibhav.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by vaibhav on 10/8/16.
+ */
+public class Tube {
+
+    public static final int TUBE_WIDTH = 52;
+    private static final int TUBE_GAP = 100; //opening between tubes
+    private static final int LOWEST_OPENING = 120; //lowest position the top of the bottom tube can be, must be above 90 to be above ground level
+    private static final int FLUCTUATION = 130;  //may adjust to keep top tube in view
+    private Texture topTube,bottomTube;
+    private Vector2 posTopTube,posBottomTube;
+    private Rectangle boundsTop, boundsBottom;
+
+    private Random random;
+
+
+    public Tube(float x) {
+
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        random = new Random();
+        posTopTube = new Vector2(x, random.nextInt(FLUCTUATION) + LOWEST_OPENING + TUBE_GAP);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBottom = new Rectangle(posBottomTube.x, posBottomTube.y, bottomTube.getWidth(), bottomTube.getHeight());
+
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+
+    public void reposition(float x)
+    {
+        posTopTube.set(x, random.nextInt(FLUCTUATION) + LOWEST_OPENING + TUBE_GAP);
+        posBottomTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+        boundsTop.setPosition(x, posTopTube.y);
+        boundsBottom.setPosition(x, posBottomTube.y);
+    }
+
+    public boolean collides(Rectangle player){
+        return player.overlaps(boundsBottom) || player.overlaps(boundsTop);
+    }
+
+}
--- core/src/com/vaibhav/game/states/PlayState.java
+++ core/src/com/vaibhav/game/states/PlayState.java
@@ -1,32 +1,63 @@
 package com.vaibhav.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.vaibhav.game.MyGdxGame;
+import com.vaibhav.game.sprites.Bird;
+import com.vaibhav.game.sprites.Tube;
 
 /**
  * Created by vaibhav on 5/8/16.
  */
 public class PlayState extends State {
 
-    private Texture bird;
+    private Bird bird;
+    private Texture background;
+    private final static int TUBE_SPACING = 100;
+    private static final int TUBE_COUNT = 4;
 
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gameStateManager) {
         super(gameStateManager);
-        bird = new Texture("bird.png");
-        camera.setToOrtho(false,MyGdxGame.WIDTH/2,MyGdxGame.HEIGHT/2);
+        bird = new Bird(50, 300);
+        camera.setToOrtho(false, MyGdxGame.WIDTH / 2, MyGdxGame.HEIGHT / 2);
+        background = new Texture("bg.png");
+        tubes = new Array<Tube>();
+        for (int i = 1; i <= TUBE_COUNT; i++) {
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
+
 
     }
 
     @Override
     protected void handleInput() {
 
+        if (Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
     public void update(float deltaTime) {
+        handleInput();
+        bird.update(deltaTime);
+        camera.position.x = bird.getPosition().x + 100;
 
+        for (Tube tube : tubes) {
+            if (camera.position.x - camera.viewportWidth / 2 > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+
+            if (tube.collides(bird.getBounds())) {
+                gsm.setState(new MenuState(gsm));
+            }
+
+        }
+        camera.update();
     }
 
     @Override
@@ -34,7 +65,13 @@ public class PlayState extends State {
 
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
-        spriteBatch.draw(bird, 50,50);
+        spriteBatch.draw(background, camera.position.x - (camera.viewportWidth / 2), 0);
+        spriteBatch.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        for (Tube tube : tubes) {
+            spriteBatch.draw(tube.getBottomTube(), tube.getPosBottomTube().x, tube.getPosBottomTube().y);
+            spriteBatch.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        }
+
         spriteBatch.end();
 
     }
